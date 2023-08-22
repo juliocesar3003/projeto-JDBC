20a3aaea66a02ededf4b603cf3da140cc4347dbb
julio <julio.cesar.roseli@gmail.com>
2023-08-22 00:34:26 -0300

    criando classe vendedorDaoJDBC E DaoFactory

---
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.VendedorDao;
+import model.entities.Vendedor;
+
+public class VendedorDaoJDBC implements VendedorDao {
+
+	@Override
+	public void insert(Vendedor obj) {
+		
+		
+	}
+
+	@Override
+	public void update(Vendedor obj) {
+	
+		
+	}
+
+	@Override
+	public void deleteById(Vendedor obj) {
+		
+		
+	}
+
+	@Override
+	public Vendedor FindById(Integer id) {
+	
+		return null;
+	}
+
+	@Override
+	public List<Vendedor> findAll() {
+	
+		return null;
+	}
+   
+}
